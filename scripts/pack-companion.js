aff529d34d626dd85d62c96c429dc37ce9e26019
Nelson Teixeira <smartchoice@smartchoice.pt>
2026-09-15 09:50:43 +0100

    v0.3.3: companion tgz with root dir (Companion strip:1 install layout)

--- scripts/pack-companion.js
+++ scripts/pack-companion.js
@@ -26,10 +26,6 @@ const manifest = {
   keywords: ['audio', 'jingles', 'playout', 'cart', 'radio', 'sound'],
 };
 
-const manifestDir = path.join(companionDir, 'companion');
-fs.mkdirSync(manifestDir, { recursive: true });
-fs.writeFileSync(path.join(manifestDir, 'manifest.json'), JSON.stringify(manifest, null, 2) + '\n');
-
 try {
   const { validateManifest } = require(path.join(companionDir, 'node_modules', '@companion-module', 'base'));
   validateManifest(manifest, false);
@@ -44,12 +40,24 @@ for (const f of fs.readdirSync(outDir)) {
   if (f.endsWith('.tgz')) fs.unlinkSync(path.join(outDir, f));
 }
 
+const rootDirName = `${manifest.id}-${manifest.version}`;
+const stagingParent = path.join(root, 'release', '.companion-staging');
+const stagingDir = path.join(stagingParent, rootDirName);
+fs.rmSync(stagingParent, { recursive: true, force: true });
+fs.mkdirSync(path.join(stagingDir, 'companion'), { recursive: true });
+fs.mkdirSync(path.join(stagingDir, 'src'), { recursive: true });
+
+fs.copyFileSync(path.join(companionDir, 'index.js'), path.join(stagingDir, 'index.js'));
+fs.copyFileSync(path.join(companionDir, 'src', 'main.js'), path.join(stagingDir, 'src', 'main.js'));
+fs.copyFileSync(path.join(companionDir, 'README.md'), path.join(stagingDir, 'README.md'));
+fs.copyFileSync(path.join(companionDir, 'LICENSE'), path.join(stagingDir, 'LICENSE'));
+fs.writeFileSync(path.join(stagingDir, 'companion', 'manifest.json'), JSON.stringify(manifest, null, 2) + '\n');
+
 const outName = `Companion module Smart Jingle (v${pkg.version}).tgz`;
 const outPath = path.join(outDir, outName);
 
-execFileSync('tar', ['-czf', outPath, 'index.js', 'src', 'README.md', 'LICENSE', 'companion/manifest.json'], {
-  cwd: companionDir,
-  stdio: 'inherit',
-});
+execFileSync('tar', ['-czf', outPath, rootDirName], { cwd: stagingParent, stdio: 'inherit' });
+
+fs.rmSync(stagingParent, { recursive: true, force: true });
 
 console.log('Companion module packed: release/companion/' + outName);
